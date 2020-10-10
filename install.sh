637b422a9a4e27f6809c67b2b626924e02e3bb0a
Loïc Delmaire <loic.delmaire@gmail.com>
2020-10-10 22:59:17 +0200

    Add ruby-version and setup rvm

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 ./symlink.sh
+rvm --default use `cat ruby-version`
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
 vim -u ~/.vimrc.bundles +BundleInstall +qa
